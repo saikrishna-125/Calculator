13f8c5ec31a1aa89db76e565d07f9becec7b802f
Sai Krishna <sai.k.karnam@gmail.com>
2026-09-06 18:01:29 -0600

    Define calculation functions

---
+++ app.js
@@ -0,0 +1,38 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+function operate(num1, num2, operator) {
+  result = 0;
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+  }
+
+  return result;
+}
