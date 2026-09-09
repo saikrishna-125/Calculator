3abe4b41e6869fac5ae0b4d200c106f118181415
Sai Krishna <sai.k.karnam@gmail.com>
2026-09-08 23:05:02 -0600

    Project logic revamp

--- app.js
+++ app.js
@@ -2,82 +2,80 @@ const buttons = document.querySelectorAll("button");
 const display = document.querySelector("#display");
 
 let operation = {
-  num1: "",
+  num1: "0",
   num2: "",
   operator: "",
 };
 
-let snarky_msg = "That ain't right!";
 let active = "num1";
 
-let hasDecimalPoint = false;
-let negative = false;
-
-display.textContent = "0";
+display.textContent = operation[active];
 
 for (let button of buttons) {
   button.addEventListener("click", () => {
     let char = button.textContent;
 
-    if (
-      isFinite(char) ||
-      (char === "." && !hasDecimalPoint) ||
-      char === "Back"
-    ) {
-      if (operation.operator === "=") {
-        reset();
-      }
-
-      if (char === "Back") {
-        backspace();
-      } else {
-        operation[active] += char;
-      }
-      display.textContent =
-        operation[active].length === 0 ? "0" : operation[active];
-
-      if (char === ".") {
-        hasDecimalPoint = true;
-      }
+    if (isFinite(char)) {
+      inputNumber(char);
     } else if ("+-x/".includes(char)) {
-      if (operation.num2) {
-        calculate(operation);
-      }
       active = "num2";
+
+      if (operation.num2) {
+        calculate();
+      }
+
       operation.operator = char;
-      hasDecimalPoint = false;
     } else if (char === "=") {
-      calculate(operation);
-      operation.operator = char;
+      calculate();
+      operation.operator = "=";
     } else if (char === "Clear") {
       reset();
-      display.textContent = "0";
+      display.textContent = operation.num1;
     }
   });
 }
 
-function calculate(operation) {
-  if (Number(operation.num2) === 0 && operation.operator === "/") {
-    divideByZero();
-  } else {
-    if (operation.num2 === "" || operation.operator === "") {
-      return operation.num1;
-    }
-
-    let result = operate(
-      Number(operation.num1),
-      Number(operation.num2),
-      operation.operator,
-    );
-    result = Math.round(result * 10 ** 8) / 10 ** 8;
-
-    operation.num1 = result;
-    display.textContent = operation.num1;
-    operation.num2 = "";
-    operation.operator = "";
-    active = "num2";
-    hasDecimalPoint = false;
+function inputNumber(char) {
+  if (operation.operator === "=") {
+    reset();
   }
+  current = operation[active];
+  if (current === "0") {
+    if (char !== "0") {
+      current = char;
+    }
+  } else {
+    current += char;
+  }
+
+  operation[active] = current;
+  display.textContent = current;
+}
+
+function calculate() {
+  if (operation.num2 !== "") {
+    if (Number(operation.num2) === 0 && operation.operator === "/") {
+      display.textContent = "That ain't right!";
+      reset();
+      return;
+    }
+    operation.num1 = String(
+      operate(
+        Number(operation.num1),
+        Number(operation.num2),
+        operation.operator,
+      ),
+    );
+  }
+  display.textContent = operation.num1;
+  operation.num2 = "";
+}
+
+function reset() {
+  operation.num1 = "0";
+  operation.num2 = "";
+  operation.operator = "";
+  active = "num1";
 }
 
 function operate(num1, num2, operator) {
@@ -96,19 +94,9 @@ function operate(num1, num2, operator) {
       result = divide(num1, num2);
       break;
   }
-  console.log(result);
+
+  result = Math.round(result * 10 ** 4) / 10 ** 4;
   return result;
-}
-
-function divideByZero() {
-  reset();
-  display.textContent = snarky_msg;
-}
-
-function backspace() {
-  if (operation[active] !== "") {
-    operation[active] = operation[active].slice(0, -1);
-  }
 }
 
 function add(num1, num2) {
@@ -125,12 +113,4 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
-}
-
-function reset() {
-  operation.num1 = "";
-  operation.num2 = "";
-  operation.operator = "";
-  active = "num1";
-  hasDecimalPoint = false;
 }
