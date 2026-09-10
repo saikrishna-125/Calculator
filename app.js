51cfb68f539fa40ba5ce7926f439e885101aa25d
Sai Krishna <sai.k.karnam@gmail.com>
2026-09-09 18:03:59 -0600

    Add comments for code readability

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const display = document.querySelector("#display");
 
 const body = document.querySelector("body");
 
+// new operation object
 let operation = {
   num1: "",
   num2: "",
   operator: "",
 };
 
+// active input
 let active = "num1";
 
+// initial screen
 display.textContent = "_";
 
+// keyboard access  - converts to relevant button press characters
 body.addEventListener("keydown", (e) => {
   let key = e.key;
 
@@ -31,6 +35,7 @@ body.addEventListener("keydown", (e) => {
   actions(key);
 });
 
+// Add event listeners for buttons
 for (let button of buttons) {
   button.addEventListener("click", () => {
     let char = button.textContent;
@@ -40,11 +45,17 @@ for (let button of buttons) {
 }
 
 function actions(char) {
+  // input handling conditions
   if (isFinite(char) || char === "." || char === "BACK" || char === "+ / -") {
     inputNumber(char);
-  } else if ("+-x/".includes(char)) {
+  }
+  // operator handling
+  else if ("+-x/".includes(char)) {
+    // operator should not be the first input (before num1)
     if (operation.num1) {
+      // makes num2 as active input after reading an operator
       active = "num2";
+      // if both num1 and num2 are entered and a new operator is pressed - then first calculate result
       if (operation.num2) {
         calculate();
       }
@@ -60,61 +71,85 @@ function actions(char) {
 }
 
 function inputNumber(char) {
+  // if a number is entered after pressing "=", then a new calculator should start
   if (operation.operator === "=") {
     reset();
   }
+
   current = operation[active];
 
+  // add period only if not already present
   if (char === ".") {
+    // if . is first character then add 0 before it
     if (current === "") {
       current = "0.";
     }
     if (!current.includes(char)) {
       current += char;
     }
-  } else if (char === "BACK") {
+  }
+  // backspace - remove last entered value, but if result is empty then change to 0
+  else if (char === "BACK") {
     current = current.slice(0, -1);
     if (current === "") {
       current = "0";
     }
-  } else if (char === "+ / -") {
+  }
+  // toggle input symbol (+ / -)
+  else if (char === "+ / -") {
     if (!current.startsWith("-")) {
       current = "-" + current;
+
+      // if - is the only character then change to -0
       if (current === "-") {
         current = "-0";
       }
     } else {
+      // remove -
       current = current.slice(1);
     }
-  } else {
+  }
+  // new number character
+  else {
+    // remove zero on first character
     if (current === "0") {
+      // don't allow user to enter multiple zeroes
       if (char !== "0") {
         current = char;
       }
-    } else if (current === "-0") {
+    }
+    // remove zero on first character while preserving negative '-' symbol
+    else if (current === "-0") {
       current = "-" + char;
     } else {
+      // add char to current input number
       current += char;
     }
   }
 
+  // update object and display
   operation[active] = current;
   display.textContent = current;
 }
 
 function calculate() {
+  // if num1 is empty or only -, then num1 should be 0
   if (operation.num1 === "-" || operation.num1 === "") {
     operation.num1 = "0";
   }
   if (operation.num2 === "-") {
     operation.num2 = "0";
   }
+  // should only perform operation if num2 is provided, else result is  num1
   if (operation.num2 !== "") {
+    // divide by zero handler
     if (Number(operation.num2) === 0 && operation.operator === "/") {
       display.textContent = "That ain't right!";
       reset();
       return;
     }
+
+    // calculate result and store it as num1
     operation.num1 = String(
       operate(
         Number(operation.num1),
@@ -123,6 +158,8 @@ function calculate() {
       ),
     );
   }
+
+  // update display and reset num2
   display.textContent = operation.num1;
   operation.num2 = "";
 }
